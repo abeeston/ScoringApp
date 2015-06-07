c33d37b8d93bc936a7f465fd9ca9e8159de5104f
Michael <sekimworld03@yahoo.com>
2015-06-06 18:35:08 -0600

    This is the first commit from Michael!

--- app/src/main/java/com/example/amy/scoringapp/MainActivity.java
+++ app/src/main/java/com/example/amy/scoringapp/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends ActionBarActivity {
 
 
         System.out.println("This is a test from Amy");
+        System.out.println("This is a test from Michael The Great!");
     }
 
     @Override
