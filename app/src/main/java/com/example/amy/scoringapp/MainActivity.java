bedbe0739113fec5abce6a669c6d62aa13c2f66b
Mark Hall <mhall0717@yahoo.com>
2015-06-06 18:25:30 -0600

    Mark Hall.  Added some sweet comments.

--- app/src/main/java/com/example/amy/scoringapp/MainActivity.java
+++ app/src/main/java/com/example/amy/scoringapp/MainActivity.java
@@ -28,7 +28,8 @@ public class MainActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as you specify a parent activity in AndroidManifest.xml
+        // Commit this.  Yeahhhhh....
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
