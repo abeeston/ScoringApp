ccbc1f3caa9995ddd4dc188006ebbe736504a0fa
Michael <sekimworld03@yahoo.com>
2015-06-13 14:26:51 -0600

    added Tournament and database observer

---
+++ app/src/main/java/com/example/amy/scoringapp/DatabaseObserver.java
@@ -0,0 +1,10 @@
+package com.example.amy.scoringapp;
+
+/**
+ * Created by Trevor on 6/13/2015.
+ */
+public interface DatabaseObserver {
+    public String pullData();
+    public void pushData();
+    public void display();
+}
