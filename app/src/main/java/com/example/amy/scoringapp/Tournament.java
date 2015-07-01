d86aa6629841e506dd257cd3636d5f49c66c572e
mgwill03 <sekimworld03@gmail.com>
2015-07-01 13:19:15 -0700

    Tournament class is now correctly documented for JavaDoc

--- app/src/main/java/com/example/amy/scoringapp/Tournament.java
+++ app/src/main/java/com/example/amy/scoringapp/Tournament.java
@@ -14,7 +14,9 @@ import java.util.Map;
  * push data and keep data constant between this and Firebase.
  */
 public class Tournament implements DatabaseObserver {
-    //default constructor
+    /**
+     * default constructor
+     */
     public Tournament() {
         this.date = null;
         this.city = null;
@@ -22,7 +24,12 @@ public class Tournament implements DatabaseObserver {
         this.games = null;
     }
 
-    //Non default constructor. Games must be added after creation.
+    /**
+     * Non default constructor. Games must be added after creation.
+     * @param date Starting date of tournament
+     * @param city Tournament city
+     * @param password password needed to alter tournament
+     */
     public Tournament(String date, String city, String password) {
         this.date = date;
         this.city = city;
@@ -30,7 +37,13 @@ public class Tournament implements DatabaseObserver {
         this.games = null;
     }
 
-    //Non default constructor for loading from the database
+    /**
+     * Non default constructor for loading from the database
+     * @param ID Unique ID to be used in Firebase
+     * @param date Starting date of tournament
+     * @param city Tournament city
+     * @param password password needed to alter tournament
+     */
     public Tournament(String ID, String date, String city, String password) {
         this.ID = ID;
         this.date = date;
@@ -47,41 +60,33 @@ public class Tournament implements DatabaseObserver {
     private String password;
 
     //All standard getters and setters
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getDate() {
-        return date;
-    }
-    public void setDate(String date) {
-        this.date = date;
-    }
-    public String getID() {
-        return ID;
-    }
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-    public List<String> getGames() {
-        return games;
-    }
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+    public String getID() { return ID; }
+    public void setID(String ID) { this.ID = ID; }
+    public List<String> getGames() { return games; }
     public void setGames(List<String> games) { this.games = games; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-
+    
+    /**
+     * Update class with data from Firebase
+     * @param url url to the Firebase table
+     */
     @Override
-    //Update class with data from firebase
     public void pullData(Firebase url) {
         //we may or may not need this class
         //I need to understand more how Marks
         //load tournament class works.
     }
 
+    /**
+     * update Firebase with data from class
+     * @param url url to the Firebase table
+     */
     @Override
-    //update Firebase with data from class
     public void updateData(Firebase url) {
         //creates reference to Tournments
         Firebase ref = url.child("Tournaments");
@@ -92,8 +97,11 @@ public class Tournament implements DatabaseObserver {
         ref.child("Tournaments/" + ID + "/password").setValue(password);
     }
 
+    /**
+     * Create New Firebase table slot and add class data
+     * @param url url to the Firebase table
+     */
     @Override
-    //Create New Firebase table slot and add class data
     public void pushData(Firebase url) {
         //creates reference to Tournaments
         Firebase ref = url.child("Tournaments");
@@ -114,6 +122,10 @@ public class Tournament implements DatabaseObserver {
         ID = newPostRef.getKey();
     }
 
+    /**
+     * Create new Firebase table slot and add class data
+     * @return displayable string with city and date
+     */
     @Override
     public String display() {
         //return "Test!";
