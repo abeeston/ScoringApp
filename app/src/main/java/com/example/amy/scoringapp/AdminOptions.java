7e3ab0971253184a49da873d28b3a5cc4fdab79e
Mark Hall <mhall0717@yahoo.com>
2015-07-08 22:59:03 -0600

    Added a few new screens and connected them.  Still haven't added actually editing the tournament yet.

--- app/src/main/java/com/example/amy/scoringapp/AdminOptions.java
+++ app/src/main/java/com/example/amy/scoringapp/AdminOptions.java
@@ -1,40 +1,28 @@
 package com.example.amy.scoringapp;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-/**
- *  Handles the admin login
- */
-public class AdminLogin extends ActionBarActivity {
+
+public class AdminOptions extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_login);
+        setContentView(R.layout.activity_admin_options);
     }
 
-    /**
-     * Creates the options menu
-     * @param menu The menu
-     * @return Boolean
-     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_admin_login, menu);
+        getMenuInflater().inflate(R.menu.menu_admin_options, menu);
         return true;
     }
 
-    /**
-     * Returns the item selected
-     * @param item The item
-     * @return Boolean
-     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -49,13 +37,13 @@ public class AdminLogin extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void onClickAddOrDelete(View view) {
+        Intent intent = new Intent(this, AdminAddOrDelete.class);
+        startActivity(intent);
+    }
 
-    /**
-     * Goes into the next screen and passes the information to it.
-     * @param view The view
-     */
-    public void onClickAdminLogin(View view) {
-        Intent intent = new Intent(this, AdminOptions.class);
+    public void onClickAdminOptions(View view) {
+        Intent intent = new Intent(this, AdminActivity.class);
         startActivity(intent);
     }
 }
